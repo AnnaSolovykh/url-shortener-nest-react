f29fdcca951eedbd597f0f2c032400613922fb9c
Anna Solovykh <ms.teachy.solo@gmail.com>
2025-06-20 23:59:05 -0400

    feat: add docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost', 'http://localhost:5173'],
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
